dc3953ef787b96b8f75673b7be40af03d0e4c1ec
Jack Leow <jackgene@gmail.com>
2012-07-10 21:29:00 -0700

    Applied some Scala/Hadoop best practices.

--- src/main/scala/my/edu/clhs/maths/hadoop/CalculateStatisticsJob.scala
+++ src/main/scala/my/edu/clhs/maths/hadoop/CalculateStatisticsJob.scala
@@ -29,20 +29,34 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
 import org.apache.hadoop.mapreduce.Job
 import org.apache.hadoop.mapreduce.Mapper
 import org.apache.hadoop.mapreduce.Reducer
+import org.apache.hadoop.util.GenericOptionsParser
 import org.apache.hadoop.util.Tool
 import org.apache.hadoop.util.ToolRunner
 
 import my.edu.clhs.maths._
 
 /**
+ * Simple job that calculates the statistics (currently count, mean
+ * and standard deviation) of a list of numbers.
+ * 
  * @author Jack Leow
  */
 class CalculateStatisticsJob extends Configured with Tool {
   override def run(args: Array[String]): Int = {
-    if (args.length != 2) {
-      throw new IllegalArgumentException("Usage: job input_path output_path")
+    val optsParser = new GenericOptionsParser(args)
+    
+    val (inputPath, outputPath) = optsParser.getRemainingArgs match {
+      case Array(inputPath, outputPath) =>
+        (inputPath, outputPath)
+      case _ =>
+        throw new IllegalArgumentException("Usage: job input_path output_path")
     }
-    val job = new Job()
+    
+    val job = new Job(
+      optsParser.getConfiguration(),
+      "scala-maths CalculateStatisticsJob"
+    )
+    job.setJarByClass(getClass())
     
     job.setMapperClass(classOf[CalculateStatisticsJob.CalcStatsMapper])
     job.setCombinerClass(classOf[CalculateStatisticsJob.CalcStatsReducer])
@@ -51,8 +65,8 @@ class CalculateStatisticsJob extends Configured with Tool {
     job.setOutputKeyClass(classOf[NullWritable])
     job.setOutputValueClass(classOf[DoubleStatistics.Writable])
     
-    FileInputFormat.addInputPath(job, new Path(args(0)))
-    FileOutputFormat.setOutputPath(job, new Path(args(1)))
+    FileInputFormat.addInputPath(job, new Path(inputPath))
+    FileOutputFormat.setOutputPath(job, new Path(outputPath))
     
     job.waitForCompletion(false)
     return 0
@@ -79,7 +93,7 @@ object CalculateStatisticsJob {
       context.write(nullKey, allStats.map(_.get).reduce(_ + _))
     }
   }
-
+  
   def main(args : Array[String]) : Unit =
     ToolRunner.run(new CalculateStatisticsJob(), args)
 }
